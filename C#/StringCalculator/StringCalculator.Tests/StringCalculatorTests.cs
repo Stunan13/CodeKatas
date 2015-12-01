5f641a5e2aa322dcc906bebfba6cc06225afff9b
Stunan13 <stunan13@gmail.com>
2015-12-01 16:41:03 +0000

    Added StringCalculator

---
+++ C#/StringCalculator/StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Security.Cryptography;
+using NUnit.Framework;
+using StringCalculator;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        #region Helper Methods
+        private static StringCalculator MakeStringCalculator()
+        {
+            return new StringCalculator();
+        }
+        #endregion
+
+        [Test]
+        public void Add_ReturnsZero_WithEmptyStringInput()
+        {
+            var calc = MakeStringCalculator();
+
+            var expected = 0;
+            var actual = calc.Add("");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("0", 0)]
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        public void Add_ReturnsParameterAsNumber_WithOneNumberInput(string input, int expected)
+        {
+            var calc = MakeStringCalculator();
+
+            var actual = calc.Add(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("0, 0", 0)]
+        [TestCase("2, 1, 1", 4)]
+        [TestCase("2, 1, 1, 5", 9)]
+        [TestCase("1, 2, 3, 4, 5", 15)]
+        public void Add_ReturnsCorrectSum_OfAnyNumbersDelimitedByCommas(string input, int expected)
+        {
+            var calc = MakeStringCalculator();
+
+            var actual = calc.Add(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestCase("2\n 1", 3)]
+        [TestCase("2\n 2\n 3", 7)]
+        [TestCase("2\n 1\n 1\n 5", 9)]
+        [TestCase("1\n 2\n 3\n 4\n 5", 15)]
+        public void Add_ReturnsCorrectSum_OfAnyNumbersDelimitedByNewLine(string input, int expected)
+        {
+            var calc = MakeStringCalculator();
+
+            var actual = calc.Add(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("0\n 0", 0)]
+        [TestCase("0, 1", 1)]
+        [TestCase("2\n 1, 1", 4)]
+        [TestCase("2, 2\n 3", 7)]
+        [TestCase("2\n 1, 1\n 5", 9)]
+        [TestCase("1, 2\n 3, 4\n 5", 15)]
+        public void Add_ReturnsCorrectSum_OfAnyNumbersDelimitedByNewLineOrCommas(string input, int expected)
+        {
+            var calc = MakeStringCalculator();
+
+            var actual = calc.Add(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(",.")]
+        [TestCase("0, 1, 2, -")]
+        [TestCase("1, 3, f")]
+        [TestCase("4, !")]
+        public void Add_ThrowsArgurmentException_WhenInputContainsNonIntegerValues(string input)
+        {
+            var calc = MakeStringCalculator();
+
+            var ex = Assert.Throws<ArgumentException>(() => calc.Add(input));
+            var expected = "is not a valid whole number";
+
+            StringAssert.Contains(expected, ex.Message);
+        }
+
+        [TestCase(",\n2, 3", 5)]
+        [TestCase("1,,, 2,", 3)]
+        [TestCase("1, 3, \n\n", 4)]
+        [TestCase("4, \n,,\n", 4)]
+        public void Add_SkipsEmptyParams_WhenInputContainsThem(string input, int expected)
+        {
+            var calc = MakeStringCalculator();
+
+            var actual = calc.Add(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ C#/StringCalculator/StringCalculator/StringCalculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        /// <summary>
+        /// Adds together numbers delimited by commas e.g. "0,1,2,3"
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public object Add(string input)
+        {
+            int sum = 0;
+            var delimiters = GetDelimiters(input);
+
+            if (input != string.Empty)
+            {
+                string[] numbers = input.Replace(" ", "")
+                                        .Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var number in numbers)
+                {
+                    int numToAdd = 0;
+                    if (int.TryParse(number, out numToAdd))
+                    {
+                        sum += int.Parse(number);
+                    }
+                    else
+                    {
+                        throw new ArgumentException(string.Format("Argument: {0} is not a valid whole number", number));
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        private char[] GetDelimiters(string input)
+        {
+            char[] delimiters = new char[] { ',', '\n' };
+            var regex = new Regex(@"/^(\/\/).+(?:\\n)/g");
+
+            if (regex.IsMatch(input))
+            {
+                Match m = regex.Match(input);
+                //delimiters = new char[] { m.Value.Remove(0, 2).Replace("\n", "").ToCharArray() };
+            }
+
+            return delimiters;
+        }
+    }
+}
